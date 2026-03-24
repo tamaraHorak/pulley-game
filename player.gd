5769a32636cde228a82bf77bac8fa3b369305a60
MaPatRou <alex.huck@gmx.ch>
2026-03-24 17:48:51 +0100

    lalala

--- player.gd
+++ player.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends CharacterBody2D
 
 @export var speed = 200 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
@@ -9,15 +9,15 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	var velocity = Vector2.ZERO # The player's movement vector.
+func _physics_process(delta):
+	velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+	move_and_slide()
