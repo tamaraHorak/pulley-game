03df11adc8729d5cb276e05d732f48d099132d1d
tamaraHorak <tamara.horak@epfl.ch>
2026-03-31 14:35:39 +0200

    added all the features pof the player

--- player.gd
+++ player.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
 
-@export var speed = 200 # How fast the player will move (pixels/sec).
+@export var speed = 200 # How fast the player will move (pixels/sec)
+@export var jump_force = -400
 var screen_size # Size of the game window.
+var is_crouching = false
+
+const GRAVITY = 800
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,14 +14,34 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+
+	if Input.is_action_pressed("crouch") and is_on_floor():
+		is_crouching = true
+		$AnimatedSprite2D.play("crouching")
 	else:
-		$AnimatedSprite2D.stop()
+		is_crouching = false
+
+	if not is_crouching:
+		if Input.is_action_pressed("move_right"):
+			velocity.x = speed
+		elif Input.is_action_pressed("move_left"):
+			velocity.x = -speed
+		else:
+			velocity.x = 0
+
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = jump_force
+
+		if not is_on_floor():
+			$AnimatedSprite2D.play("jumping")
+		elif velocity.x != 0:
+			$AnimatedSprite2D.play("walking")
+		else:
+			$AnimatedSprite2D.stop()
+
+		if velocity.x != 0:
+			$AnimatedSprite2D.flip_h = velocity.x < 0
+
 	move_and_slide()
