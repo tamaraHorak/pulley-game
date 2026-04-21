d67d4dc1ffefb84521774212656ff0a1691d189e
tamaraHorak <tamara.horak@epfl.ch>
2026-04-21 15:01:55 +0200

    changed things

--- player.gd
+++ player.gd
@@ -1,12 +1,14 @@
 extends CharacterBody2D
 
-@export var speed = 200 # How fast the player will move (pixels/sec)
-@export var jump_force = -400
+@export var speed = 400 # How fast the player will move (pixels/sec)
+@export var jump_force = -550
 var screen_size # Size of the game window.
 var is_crouching = false
 var platform_velocity := Vector2.ZERO
+var has_sac = false
+var sac_scene = preload("res://sac.tscn")
 
-const GRAVITY = 800
+const GRAVITY = 1200
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,7 +17,10 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+func _physics_process(_delta):
+	var current_speed = speed / 2.0 if has_sac else speed
+	var current_jump = jump_force * 0.9 if has_sac else jump_force
+	var current_gravity = GRAVITY * 1.5 if has_sac else GRAVITY
 	# Gravity
 	if is_on_floor():
 		velocity.y = 0
@@ -27,7 +32,7 @@ func _physics_process(delta):
 			else:
 				platform_velocity = Vector2.ZERO
 	else:
-		velocity.y += GRAVITY * delta
+		velocity.y += current_gravity * _delta
 	
 	# Crouching
 	if Input.is_action_just_pressed("crouch") and is_on_floor():
@@ -38,17 +43,23 @@ func _physics_process(delta):
 	if is_crouching:
 		$CollisionCrouching.disabled = false
 		$CollisionStanding.disabled = true
-		$AnimatedSprite2D.play("crouching")
-		velocity.x = 0
+		if Input.is_action_pressed("move_right"):
+			velocity.x = current_speed / 2.0  # slower while crouching
+			$AnimatedSprite2D.flip_h = false
+			$AnimatedSprite2D.play("crouching_walk")
+		elif Input.is_action_pressed("move_left"):
+			velocity.x = -current_speed / 2.0
+			$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("crouching_walk")
+		else:
+			velocity.x = 0
+			$AnimatedSprite2D.play("crouching")
 	else:
-		$CollisionCrouching.disabled = true
-		$CollisionStanding.disabled = false
-
 		# Movement
 		if Input.is_action_pressed("move_right"):
-			velocity.x = speed
+			velocity.x = current_speed
 		elif Input.is_action_pressed("move_left"):
-			velocity.x = -speed
+			velocity.x = -current_speed
 		else:
 			velocity.x = 0
 		if  not is_on_floor():
@@ -56,7 +67,7 @@ func _physics_process(delta):
 
 
 		if Input.is_action_just_pressed("jump") and is_on_floor():
-			velocity.y = jump_force
+			velocity.y = current_jump
 		# Flip sprite
 		if velocity.x != 0:
 			$AnimatedSprite2D.flip_h = velocity.x < 0
@@ -73,3 +84,15 @@ func _physics_process(delta):
 	
 func pick_up_sac():
 	print("pick")
+	has_sac = true
+
+func _process(delta):
+	if has_sac and Input.is_action_just_pressed("ui_accept"):
+		drop_sac()
+		
+func drop_sac():
+	has_sac = false
+	var sac = sac_scene.instantiate()
+	sac.position = position
+	get_parent().add_child(sac)
+	print("drop")
