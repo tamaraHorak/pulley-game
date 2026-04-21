7fd7728a3305fb3699bdfc6c38ac2e172aed3d2a
jiayizhngepfl <j.zhang@epfl.ch>
2026-04-21 15:12:18 +0200

    sac

--- sac.gd
+++ sac.gd
@@ -1,19 +1,25 @@
-extends Area2D
+extends CharacterBody2D
 
 var player_nearby = false
 var player_ref = null
+const GRAVITY = 800
 
 func _ready():
-	body_entered.connect(_on_body_entered)
-	body_exited.connect(_on_body_exited)
+	$Area2D.body_entered.connect(_on_body_entered)
+	$Area2D.body_exited.connect(_on_body_exited)
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+	move_and_slide()
 
 func _on_body_entered(body):
-	if body is CharacterBody2D:
+	if body is CharacterBody2D and body.name == "Player":
 		player_nearby = true
 		player_ref = body
 
 func _on_body_exited(body):
-	if body is CharacterBody2D:
+	if body is CharacterBody2D and body.name == "Player":
 		player_nearby = false
 		player_ref = null
 
