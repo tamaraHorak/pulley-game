55eecc5a8f06ca4297f4abbc97022c7e3ee3aef0
MaPatRou <alex.huck@gmx.ch>
2026-05-12 13:20:53 +0200

    my changes

--- door.gd
+++ door.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
+@export var next := "res://Back_4.tscn"
+
 func _ready():
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
 	if body.name == "Player":
 		print("Congrats")
-		get_tree().change_scene_to_file("res://level_1.tscn")  
+		get_tree().change_scene_to_file(next)  
