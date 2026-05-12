29f3b7c3e4e1e8c0cf2e7ce40a7da0a473f3e163
AlbanSib <you@example.com>
2026-05-12 13:29:13 +0200

    force

---
+++ backup.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var anim = $Fade/Fade_transition/AnimationPlayer
+@onready var fade_node = $Fade/Fade_transition
+@onready var fade_timer = $Fade/Fade_transition/fade_timer
+
+func _ready():
+	anim.play("fade_out")
+	await anim.animation_finished
+	fade_node.hide()
