2fd500e725762ad583c54751bb43a40960474d52
AlbanSib <you@example.com>
2026-05-26 14:26:48 +0200

    corrections de bugs

--- end_screen.gd
+++ end_screen.gd
@@ -4,6 +4,8 @@ extends Control
 @onready var time_label = $TimeLabel
 
 func _ready():
+	await get_tree().process_frame  
+	await UIManager.fade_in()
 	GameManager.stop_timer()
 	congratulations_label.text = "Congratulations!"
 	time_label.text = "Your Time: " + GameManager.get_time_string()
--- game_manager.gd
+++ game_manager.gd
@@ -1,9 +1,11 @@
 extends Control
 
+var timer_running := false
+var timer_started := false
 
-var timer_running = false
-var start_time = 0.0
-var elapsed_time = 0.0
+var start_time := 0.0
+var elapsed_time := 0.0
+
 
 func _process(delta):
 	if timer_running:
@@ -11,21 +13,35 @@ func _process(delta):
 
 
 func start_timer():
-	start_time = Time.get_ticks_msec() / 1000.0
+
+	# Empêche de relancer le timer
+	if timer_started:
+		return
+
+	timer_started = true
 	timer_running = true
+
+	start_time = Time.get_ticks_msec() / 1000.0
+	elapsed_time = 0.0
+
+
+func stop_timer():
+	if timer_running:
+		elapsed_time = Time.get_ticks_msec() / 1000.0 - start_time
+		timer_running = false
+
 
 func reset_timer():
 	timer_running = false
+	timer_started = false
+
 	start_time = 0.0
 	elapsed_time = 0.0
 
-func stop_timer():
-	elapsed_time = Time.get_ticks_msec() / 1000.0 - start_time
-	timer_running = false
 
-
-func get_time_string():
+func get_time_string() -> String:
 	var minutes = int(elapsed_time / 60)
 	var seconds = int(elapsed_time) % 60
 	var ms = int((elapsed_time - int(elapsed_time)) * 100)
+
 	return "%02d:%02d:%02d" % [minutes, seconds, ms]
